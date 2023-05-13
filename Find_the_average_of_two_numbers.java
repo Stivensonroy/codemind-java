3038f924e57d93437bbeedf20aa9cbf376c89da4
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2023-05-14 00:55:37 +0900

    import java.util.*; public class Aver {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         Float m,n,a;         m=sc.nextFloat();         n=sc.nextFloat();         a=(m+n)/2;         System.out.format("%.4f",a);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Aver
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Float m,n,a;
+        m=sc.nextFloat();
+        n=sc.nextFloat();
+        a=(m+n)/2;
+        System.out.format("%.4f",a);
+    }
+}
