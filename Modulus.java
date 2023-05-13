7c837296cd046d69727d39dc8a699aaaf1fec574
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2023-05-14 00:19:52 +0900

    import java.util.*; public class Numb {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int m,n,d;         m=sc.nextInt();         n=sc.nextInt();         d=m%n;         System.out.println(d);
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Numb
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int m,n,d;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        d=m%n;
+        System.out.println(d);
+        
+    }
+}
