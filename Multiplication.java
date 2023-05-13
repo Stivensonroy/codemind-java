80d082ecd5ceafe0351933c2cabfc837ce2d9e1b
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2023-05-14 00:27:12 +0900

    import java.util.*; public class Sample {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int m,n,d;         m=sc.nextInt();         n=sc.nextInt();         d=m*n;         System.out.println(d);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int m,n,d;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        d=m*n;
+        System.out.println(d);
+    }
+}
